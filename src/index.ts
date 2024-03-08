d472e2c7a79fba505cdeb560b4b8968472d44e81
Tiago <T@t.com>
2024-03-08 22:32:06 +0000

    refs #1 - update

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,8 @@
 export * from './hello';
 export * from './folder';
 
+/**
+ * @summary stroes the current package version
+ * @memberOf module:ts-workspace
+ */
 export const VERSION = "##VERSION##"
